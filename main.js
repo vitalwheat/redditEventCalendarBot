9b7375b6f145bf1788666ca2e36eb24bcf2b0cff
Your Name <you@example.com>
2020-04-02 05:42:00 -0700

    added npm fetch

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const data = require('./config.json');
-console.log(data.redditSecret);
+console.log(data.appSecret);
+
+
+/* const reddit = require('reddit');
+
+const reddit = new reddit({
+  username: 'imWheat',
+  password: 'myPassword123',
+  appId: 'eventCalendar',
+  appSecret: 'd7dFD76Fkhjd9879skdf8',
+  userAgent: 'MyApp/1.0.0 (http://example.com)'
+}) */
