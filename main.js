b25665b9289244ba5a7c72a327b98952d35a0c58
Your Name <you@example.com>
2020-04-11 17:27:46 -0700

    first commit

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ let url = "https://www.reddit.com/r/patest/new/.json";
 let settings = {
   method: "Get"
 };
+const eventMentioned = "[event]";
+const snoowrap = require('snoowrap');
+const r = new snoowrap({
+  username: data.userName,
+  password: data.password,
+  userAgent: data.userAgent,
+  clientId: data.clientId,
+  clientSecret: data.clientSecret
+});
 
 //sqlite setup
 const sqlite3 = require('sqlite3').verbose();
@@ -21,12 +30,7 @@ fetch(url, settings)
   .then(res => res.json())
   .then((json) => {
     json.data.children.forEach((element) => {
-      var alreadyProcessed = await checkIfProcessed(element.data.name, db);
-      console.log(alreadyProcessed);
-      if (alreadyProcessed == false) {
-        console.log("hello?");
-        var processedPostResult = processPost(element.data, db);
-      }
+      checkIfProcessed(element.data, db, r);
     })
     // do something with JSON
   });
@@ -37,17 +41,37 @@ db.close;
 //Takes Reddit unique identifier 
 //Returns true if post has been added to database
 //Returns true if post has NOT been added to database
-function checkIfProcessed(name, db) {
-  db.get("SELECT name FROM redditPost WHERE name='" + name + "' AND processed='1'", function (error, postId) {
-    if (postId) {
-      return true;
-    } else {
-      console.log("returning false");
-      return false;
+function checkIfProcessed(redditData, db, r) {
+  db.get("SELECT name FROM redditPost WHERE name='" + redditData.name + "' AND processed='1'", function (error, postId) {
+    if (!postId) {
+      console.log("Processing post");
+      checkIfEvent(redditData, db, r);
     }
   });
 }
 
-function processPost(redditPost, db) {
-  console.log(redditPost);
+function checkIfEvent(redditData, db, r) {
+  if (redditData.title.toLowerCase().includes(eventMentioned)) {
+    sendModMailAlert(redditData, db, r);
+    console.log(redditData.title);
+  } else {
+    db.run("INSERT INTO redditPost (name, processed) VALUES ('" + redditData.name + "', '1')");
+  }
+}
+//Sends message to modmail alerting of event post
+function sendModMailAlert(redditData, db, r) {
+  r.createModmailDiscussion({
+    body: 'test body',
+    subject: 'test subject',
+    srName: 'patest'
+  }).then(saveModMailId);
+
+  //r.getSubmission('2np694').author.name.then(console.log);
+}
+
+//save message id to database
+function saveModMailId(modMailId) {
+  var rawJson = JSON.stringify(modMailId);
+  var parsedJson = JSON.parse(rawJson);
+  console.log(parsedJson.id);
 }
